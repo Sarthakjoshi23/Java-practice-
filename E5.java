39e3f8bc20b1691cff6b7f7995e7ec6e5888ebd2
Sarthakjoshi23 <127620771+Sarthakjoshi23@users.noreply.github.com>
2023-03-13 23:58:17 +0530

    Create E5.java

---
+++ E5.java
@@ -0,0 +1,9 @@
+class E5{
+	public static void main(String[] args) {
+		System.out.println(1);
+		System.out.println(1.2);
+		System.out.println('A');
+		System.out.println('#');
+		System.out.println("ABC");
+	}
+}
